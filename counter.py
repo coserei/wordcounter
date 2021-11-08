f5445ff788b61b2f5dd939cbf06ab839d74b7068
Sourfruit <hello@def-not-hacking-the.net>
2021-11-08 17:20:38 -0500

    uwu little fuckers can meow now how many w-words they wrote!
    owO
    
    twitterw~

--- counter.py
+++ counter.py
@@ -1,5 +1,9 @@
 # Coserei 08/11/2021 (European date)
+import time
 
 TextInput = input('insert text:')
 wordcount = len(TextInput.split(' '))
 print('The total amount of words is: ' + wordcount)
+time.sleep(2)
+intent = 'https://twitter.com/intent/tweet?via=coserei&text=' + wordcount + '%20fucking%20words%21%20Tweeted%20via%20the%20wordcounter,'
+print('Tweet your results here: ' + intent)
